4ec2b0a4133819bb1720d417f34f4c2726bff5c3
seunghyun <ddock4you@gmail.com>
2021-03-03 22:32:37 +0900

    - sequelize 설정 (user, comment Model 추가)

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,18 +2,28 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const morgan = require('morgan');
 const path = require('path');
+const {
+    sequelize
+} = require('./models');
 
 const app = express();
 const page = require('./routes');
 
 const PORT = 3001;
 
-app.use(morgan('dev'));
 app.set('view engine', 'html');
 nunjucks.configure('views', {
     express: app,
     watch: true
 });
+sequelize.sync({
+    force: false
+}).then(() => {
+    console.log('database connected');
+}).catch((err) => {
+    console.error(err);
+});
+app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({
@@ -21,6 +31,13 @@ app.use(express.urlencoded({
 }));
 
 app.use('/', page);
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 app.listen(PORT, (req, res) => {
     console.log(`connect`);
--- nodejs/models/index.js
+++ nodejs/models/index.js
@@ -1,29 +1,22 @@
 const Sequelize = require('sequelize');
+const User = require('./user');
+const Comment = require('./comment');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config');
+const config = require('../config/config')[env];
 const db = {};
 
-const sequelize = new Sequelize(process.env[config.use_env_variable], config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+
+db.User = User;
+db.Comment = Comment;
+
+User.init(sequelize);
+Comment.init(sequelize);
+
+User.associate(db);
+Comment.associate(db);
 
 module.exports = db;
