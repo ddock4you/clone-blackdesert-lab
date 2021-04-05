d0e2b9a2da0dda9a16bc808d1fff1eea6d15e9b8
ddock4you <ddock4you@gmail.com>
2021-04-05 14:21:13 +0900

    - 회원가입 유효성 검사 코드 추가

--- nodejs/public/dist/js/join.js
+++ nodejs/public/dist/js/join.js
@@ -1,13 +1,31 @@
 "use strict";
 
-console.log('join');
-document.querySelector('[name=join_form]').addEventListener('submit', function (e) {
+console.log("join");
+document.querySelector("[name=join_form]").addEventListener("submit", function (e) {
   e.preventDefault();
-  var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; // 회원가입 유효성 검사
 
   if (!emailRegex.test(e.target.email.value)) {
     alert("올바른 이메일을 입력해주세요.");
     e.target.email.focus();
+    return;
+  }
+
+  if (!e.target.password.value) {
+    alert("비밀번호를 입력해주세요.");
+    e.target.password.focus();
+    return;
+  }
+
+  if (!e.target.password2.value) {
+    alert("비밀번호 확인 부분을 입력해주세요.");
+    e.target.password2.focus();
+    return;
+  }
+
+  if (e.target.password.value !== e.target.password2.value) {
+    alert("패스워드가 일치하지 않습니다. 패스워드를 확인해주세요.");
+    e.target.password2.focus();
     return;
   }
 
@@ -16,4 +34,14 @@ document.querySelector('[name=join_form]').addEventListener('submit', function (
     e.target.name.focus();
     return;
   }
+
+  axios.post("/user/join", {
+    email: e.target.email.value,
+    password: e.target.password.value,
+    birth: "".concat(e.target.birth_year, "-").concat(e.target.birth_month, "-").concat(e.target.birth_day)
+  }).then(function (response) {
+    console.log(response);
+  })["catch"](function (error) {
+    console.log(error);
+  });
 });
